502dcf2c2130c2f6384d60585ddb8320ddc54d8e
nattakonjpg <nattakonpu@kkumail.com>
2023-12-12 11:02:32 +0700

    import argparse and create function

--- testpy.py
+++ testpy.py
@@ -1,3 +1,14 @@
+import argparse 
+
+def parse_input():
+    parser = argparse.ArgparseParser()
+
+    parser.add_argument(
+        '--num',
+        type=int,
+        required=True,
+        help='unput for the multiplyby9 function'
+    )
 
 def printhello(): 
     print("hell word") 
@@ -6,6 +17,8 @@ def multiplyby9(inputV):
     print(9*inputV)
 
 if __name__=="__main__": #flow program
+    input_v = parse_input()
+    print(f'the input num is {input_v.num}')
     print("we are in the main function")
     multiplyby9(20)
     printhello()
