e6712daf9f8760726a4468516761dc512cf198fa
nattakonjpg <nattakonpu@kkumail.com>
2023-12-25 15:08:14 +0700

    del folder

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,3 +3,5 @@ import subprocess #for terminal command
 if __name__ =="__main__":
     #Basic terminal command
     subprocess.run({"ls","-ltr"}) # spacebar = ,
+    subprocess.run(["rm","-r","~/testfolder1"])
+    subprocess.run(["cd"])
