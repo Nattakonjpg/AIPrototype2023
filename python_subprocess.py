51280babc945089636629a395f7597d6290e1ec8
nattakonjpg <nattakonpu@kkumail.com>
2023-12-25 15:13:11 +0700

    add path

--- python_subprocess.py
+++ python_subprocess.py
@@ -3,5 +3,4 @@ import subprocess #for terminal command
 if __name__ =="__main__":
     #Basic terminal command
     subprocess.run({"ls","-ltr"}) # spacebar = ,
-    subprocess.run(["rm","-r","~/testfolder1"])
-    subprocess.run(["cd"])
+    subprocess.run(["rm","-r","/home/nattakonpu/testfolder1"])
