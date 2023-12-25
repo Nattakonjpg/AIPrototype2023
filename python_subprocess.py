04e7395a6d12fc89cccf9d79c07f574f374f2ba5
nattakonjpg <nattakonpu@kkumail.com>
2023-12-25 15:29:50 +0700

    run file test.py

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,5 +2,9 @@ import subprocess #for terminal command
 
 if __name__ =="__main__":
     #Basic terminal command
-    subprocess.run({"ls","-ltr"}) # spacebar = ,
-    subprocess.run(["rm","-r","/home/nattakonpu/testfolder1"])
+    subprocess.run({"ls","-ltr"}) #run "ls -ltr" "spacebar = ,"
+    subprocess.run(["rm","-r","/home/nattakonpu/testfolder1"]) #"remove folder testfolder1"
+    subprocess.run(["python","test.py","--num","5","--XX","10"])
+    subprocess.run(["python","test.py","--num","10","--XX","20"])
+    subprocess.run(["python","test.py","--num","15"])
+
