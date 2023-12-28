a55330a87bb9adcd25314c9e6ffbf66f3c900dc7
nattakonjpg <nattakonpu@kkumail.com>
2023-12-28 21:03:22 +0700

    homework

--- python_subprocess.py
+++ python_subprocess.py
@@ -14,3 +14,13 @@ if __name__ =="__main__":
     subprocess.run(["python","testpy.py","--num","0"])
     print("---------------------------------")
 
+
+    #ues output from other programk
+    process_output = subprocess.Popen(["python","testpy.py","--num","0"],
+                                        stdout=subprocess.PIPE,
+                                        stderr=subprocess.PIPE)
+    out, err = process_output,communicate()
+    print(out.decode("utf-8"))
+    print(len(out.decode("utf-8")))
+    
+    #HW write subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello word)
