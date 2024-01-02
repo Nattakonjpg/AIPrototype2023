8712d5f7411ea364673263315b9dff1aeb25aabf
nattakonjpg <nattakonpu@kkumail.com>
2024-01-02 11:28:50 +0700

    fix

--- python_subprocess.py
+++ python_subprocess.py
@@ -2,7 +2,7 @@ import subprocess #for terminal command
 
 if __name__ =="__main__":
     #Basic terminal command
-    subprocess.run({"ls","-ltr"}) #run "ls -ltr" "spacebar = ,"
+    subprocess.run(["ls","-ltr"]) #run "ls -ltr" "spacebar = ,"
     subprocess.run(["rm","-r","/home/nattakonpu/testfolder1"]) #"remove folder testfolder1"
     print("first run num=100 XX=90")
     subprocess.run(["python","testpy.py","--num","100","--XX","90"])
@@ -16,9 +16,9 @@ if __name__ =="__main__":
 
 
     #ues output from other programk
-    process_output = subprocess.Popen(["python","testpy.py","--num","0"],
-                                        stdout=subprocess.PIPE,
-                                        stderr=subprocess.PIPE)
+    process_output = subprocess.Popen(["python", "testpy.py", "--num", "0"],
+                                       stdout=subprocess.PIPE,
+                                       stderr=subprocess.PIPE)
     out, err = process_output,communicate()
     print(out.decode("utf-8"))
     print(len(out.decode("utf-8")))
