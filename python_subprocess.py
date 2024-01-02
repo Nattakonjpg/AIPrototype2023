9fc8fb2a56d4f6c216a6996a126de0efc0803c2c
nattakonjpg <nattakonpu@kkumail.com>
2024-01-02 11:48:56 +0700

    fix homework

--- python_subprocess.py
+++ python_subprocess.py
@@ -1,43 +1,24 @@
-import subprocess  # for terminal command
+#HW write subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello word)
+import subprocess
 
 def run_command(command):
-    result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
-    return result.stdout
+        result = subprocess.run(command, stdout=subprocess.PIPE, text=True)
+        return result.stdout
 
-if __name__ == "__main__":
-    # Basic terminal commands
-    subprocess.run(["ls", "-ltr"])
-    subprocess.run(["rm", "-r", "/home/nattakonpu/testfolder1"])
-
-    print("first run num=100 XX=90")
-    subprocess.run(["python", "testpy.py", "--num", "100", "--XX", "90"])
-    print("---------------------------------")
-
-    print("second run num=10 XX=90")
-    subprocess.run(["python", "testpy.py", "--num", "10", "--XX", "-90"])
-    print("---------------------------------")
-
-    print("third run num=0")
-    subprocess.run(["python", "testpy.py", "--num", "0"])
-    print("---------------------------------")
-
-    # Use output from another program
-    process_output = subprocess.Popen(["python", "testpy.py", "--num", "0"],
-                                       stdout=subprocess.PIPE,
-                                       stderr=subprocess.PIPE)
-    out, err = process_output.communicate()
-    print(out.decode("utf-8"))
-    print(len(out.decode("utf-8")))
-
-    # HW: Write subprocess sum output ทั้งหมดของ command 3 อันข้างบน (ตัวเลขก่อน Hello word)
+    if __name__ == "__main__":
     # Store the results of each command
-    results = []
+        results = []
+
+    # Basic terminal command
+        results.append(run_command(["ls", "-ltr"]))
+        results.append(run_command(["rm", "-r", "/home/nattakonpu/testfolder1"]))
 
     # Run and store the outputs of the Python scripts
-    results.append(run_command(["python", "testpy.py", "--num", "100", "--XX", "90"]))
-    results.append(run_command(["python", "testpy.py", "--num", "10", "--XX", "-90"]))
-    results.append(run_command(["python", "testpy.py", "--num", "0"]))
+        results.append(run_command(["python", "testpy.py", "--num", "100", "--XX", "90"]))
+        results.append(run_command(["python", "testpy.py", "--num", "10", "--XX", "-90"]))
+        results.append(run_command(["python", "testpy.py", "--num", "0"]))
 
     # Combine and print the results
-    combined_output = '\n'.join(results)
-    print(combined_output)
+        combined_output = '\n'.join(results)
+        print(combined_output)    
+
