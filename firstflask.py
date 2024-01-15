76a4945de19a02d16560704895631785553ba822
nattakonjpg <nattakonpu@kkumail.com>
2024-01-15 15:21:26 +0700

    fix main

--- firstflask.py
+++ firstflask.py
@@ -8,5 +8,5 @@ app = Flask(_name_)
 def helloword():
     return "Hello, World!"
 
-if __name__== "__main__"
+if __name__== "__main__":
     app.run(host='0.0.0.0',debug=True,port=50001)#host='0.0.0.0", port=500
