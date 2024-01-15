e442fbf93da6c69b94b771d3eb049a9c20163720
nattakonjpg <nattakonpu@kkumail.com>
2024-01-15 15:34:40 +0700

    5001

--- firstflask.py
+++ firstflask.py
@@ -9,4 +9,4 @@ def helloword():
     return "Hello, World!"
 
 if __name__ =="__main__":
-    app.run(host='0.0.0.0',debug=True,port=50001)#host='0.0.0.0", port=500
+    app.run(host='0.0.0.0',debug=True,port=5001)#host='0.0.0.0", port=500
